ab261ebc205dea4babd8cd4028b2348098151f26
Nicolas D <ndadevweb@gmail.com>
2025-06-30 13:47:25 +0200

    feat(lodgement_popular): List popular lodgements matching with the selected city #5

--- frontend/src/components/Lodgements/LodgementPopularList.vue
+++ frontend/src/components/Lodgements/LodgementPopularList.vue
@@ -1,11 +1,79 @@
 <script setup>
+    import LodgementPopularListItem from '@/components/Lodgements/LodgementPopularListItem.vue'
 
+    const lodgementsPopular = [
+        {
+            id: '1',
+            url : '',
+            imageSource: '/draft750x500.png',
+            imageTextAlt: 'Draft',
+            title: 'Hôtel Le soleil du matin',
+            price: '128€',
+            ranking: 5
+        },
+        {
+            id: '2',
+            url : '',
+            imageSource: '/draft750x500.png',
+            imageTextAlt: 'Draft',
+            title: "Au coeur de l'eau Chambres d'hôtes",
+            price: '71€',
+            ranking: 4
+        },
+        {
+            id: '3',
+            url : '',
+            imageSource: '/draft750x500.png',
+            imageTextAlt: 'Draft',
+            title: 'Hôtel Tout bleu et Blanc',
+            price: '68€',
+            ranking: 4
+        }
+    ]
 </script>
 
 <template>
+    <section class="lodgments-popular">
+        <h3 class="heading-section lodgments-popular-heading">Les plus populaires<i class="fa-solid fa-star"></i></h3>
 
+        <div class="lodgments-popular-cards">
+            <LodgementPopularListItem
+                v-for="lodgementPopular in lodgementsPopular"
+                :key="lodgementPopular.id"
+                :url="lodgementPopular.url"
+                :imageSource="lodgementPopular.imageSource"
+                :imageTextAlt="lodgementPopular.imageTextAlt"
+                :title="lodgementPopular.title"
+                :price="lodgementPopular.price"
+                :ranking="lodgementPopular.ranking"
+            />
+        </div>
+    </section>
 </template>
 
 <style scoped>
+.lodgments-popular {
+    --gap: 30px;
+    flex: 0 1 420px;
+    padding-bottom: 5px;
 
+    padding: 40px;
+    background: var(--color-tertiary);
+    border-radius: var(--border-radius-medium);
+    min-height: 625px;
+}
+
+.lodgments-popular-heading {
+    display: flex;
+    justify-content: space-between;
+}
+
+.lodgments-popular-heading i {
+    font-size: 21px;
+}
+
+.lodgments-popular-cards {
+    display: flex;
+    flex-flow: column wrap;
+}
 </style>
--- frontend/src/components/Lodgements/LodgementPopularListItem.vue
+++ frontend/src/components/Lodgements/LodgementPopularListItem.vue
@@ -1,11 +1,53 @@
 <script setup>
-
+defineProps({
+    url: String,
+    imageSource: String,
+    imageTextAlt: String,
+    title: String,
+    price: String,
+    ranking: Number
+})
 </script>
 
 <template>
+    <article class="lodgments-popular-card">
+        <a :href="url" class="card card-row p-5">
+            <img :src="imageSource" alt="imageTextAlt" class="card-img" />
 
+            <div class="card-details">
+                <h4 class="card-heading">{{ title }}</h4>
+                <p class="card-price">Nuit à partir de <b>{{ price }}</b></p>
+
+                <div class="card-stars">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                </div>
+            </div>
+        </a>
+    </article>
 </template>
 
 <style scoped>
+.lodgments-popular-card {
+    margin-bottom: var(--gap);
+}
 
+
+
+.card-stars {
+    margin-top: auto;
+    font-size: 11px;
+    color: var(--color-primary);
+}
+
+.card-stars i:not(:first-child) {
+    margin-left: 3px;
+}
+
+.card-stars .empty {
+    color: var(--color-tertiary);
+}
 </style>
